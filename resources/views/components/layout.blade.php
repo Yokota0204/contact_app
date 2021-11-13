8110b3aae1f8e5b802a9b971826edc9a4700cd22
Yokota0204 <yokota.02210301@gmail.com>
2021-11-13 17:08:35 +0900

    トップページを作成

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>トップページ</title>
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/'.$page.'.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ url('lib/fontawesome6.0.0/css/all.min.css') }}">
+  </head>
+  <body class="antialiased">
+    <div class="contents">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">福岡システム開発</a>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav ml-auto my-2 my-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="#">開発エンジニア紹介</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">料金</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">お問い合わせ</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <main>
+        {{ $slot }}
+      </main>
+      <footer class="bg-dark">
+        <p>営業時間：平日・土日　10:00~18:00</p>
+        <p><i class="far fa-envelope"></i>&nbsp;yokota.technology@gmail.com</p>
+        <ul class="sns-icons">
+          <li class="sns"><a href="https://twitter.com/YoheiYokota" class="sns-link"><i class="fab fa-twitter"></i></a></li>
+          <li class="sns"><a href="https://www.facebook.com/youhei.yokota.5" class="sns-link"><i class="fab fa-facebook"></i></a></li>
+          <li class="sns"><a href="https://github.com/Yokota0204" class="sns-link"><i class="fab fa-github"></i></a></li>
+        </ul>
+      </footer>
+    </div>
+  </body>
+</html>
---
+++ resources/views/top.blade.php
@@ -0,0 +1,43 @@
+<x-layout>
+  <x-slot name="page">
+    top
+  </x-slot>
+  <div class="first-impression">
+    <video type="video/mp4" src="{{ url('videos/pexels-joshua-6738974.mp4') }}" class="bg" webkit-playsinline playsinline muted autoplay loop></video>
+    <div class="text">
+      <h1 class="line">ウェブ開発のご依頼はお任せください！</h1><br>
+      <h1>プロのエンジニアが責任を持って対応させていただきます。</h1>
+    </div>
+  </div>
+  <div class="receivable-orders info-wrapper">
+    <div class="content">
+      <h2 class="title border-bottom pb-3">受注可能案件</h2>
+      <ul class="orders">
+        <li class="order">ホームページ制作（デザインはあらかじめご用意ください）</li>
+        <li class="order">Excel、Googleスプレッドシート自動化（VBA、GAS）</li>
+        <li class="order">RPA（データ入力などの単純作業自動化）</li>
+        <li class="order">社内システム開発（顧客データ管理、社内承認などをシステム化）</li>
+        <li class="order">クラウド導入サポート（ペーパーレス化）</li>
+        <li class="order">ウェブシステム開発（予約、お問い合わせ、管理などシステム化）</li>
+        <li class="order">LINEボット開発（LINE公式アカウントのシステム構築）</li>
+      </ul>
+    </div>
+  </div>
+  <div class="development-techs info-wrapper">
+    <div class="content">
+      <h2 class="title pb-3">対応技術</h2>
+      <div class="tech-part">
+        <h5>【対応言語・技術】</h5>
+        <p>HTML、CSS、JavaScript、PHP、VBA、GAS、AWS、Autoit、MySQL</p>
+      </div>
+      <div class="tech-part">
+        <h5>【対応ライブラリ/フレームワーク】</h5>
+        <p>Ruby on Rails、Laravel、Vue.js、jQuery、Bootstrap</p>
+      </div>
+      <div class="tech-part">
+        <h5>【対応API】</h5>
+        <p>LINE Messaging API、Slack</p>
+      </div>
+    </div>
+  </div>
+</x-layout>
