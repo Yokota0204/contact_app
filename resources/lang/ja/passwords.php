2011dc864c30b57eff9ed5cc00b3b85cde6ae4c9
Yokota0204 <yokota.02210301@gmail.com>
2021-12-16 15:16:24 +0900

    エラーメッセージを一部日本語に対応。

---
+++ resources/lang/ja/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'パスワードがリセットされました。',
+    'sent' => 'パスワードリセット用のリンクをメールで送信しました。',
+    'throttled' => 'しばらく時間を空けて再度お試しください。',
+    'token' => 'パスワードリセットトークンが不正です。',
+    'user' => "入力されたメールアドレスは登録されていません。",
+
+];
